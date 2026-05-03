93dab4c00fc00d0d9ea7c46641b00e8dc71ac091
zion <ox7a696f6e@outlook.com>
2026-05-04 02:36:20 +0800

    feat(nix): add nix flake for reproducible dev shell

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Octos - Agentic OS";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nix
+              nixfmt
+              statix
+
+              openssl
+              pkg-config
+              cargo
+              clippy
+              rust-analyzer
+              rustc
+              rustfmt
+            ];
+          };
+        }
+      );
+
+    };
+}
