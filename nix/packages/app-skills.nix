bfd7a602b7c36aa3780ab67ed78238cb26b0710f
zion <ox7a696f6e@outlook.com>
2026-05-09 05:24:59 +0800

    feat(nix): add app-skills package
    Build and install workspace app-skill binaries (news_fetch,
    deep-search, etc.) using rustPlatform.

---
+++ nix/packages/app-skills.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  rustPlatform,
+}:
+let
+  skillCrates = [
+    "news_fetch"
+    "deep-search"
+    "deep-crawl"
+    "send-email"
+    "account-manager"
+    "clock"
+    "weather"
+  ];
+  skillBins = [
+    "news_fetch"
+    "deep-search"
+    "deep_crawl"
+    "send_email"
+    "account_manager"
+    "clock"
+    "weather"
+  ];
+in
+rustPlatform.buildRustPackage {
+  pname = "octos-app-skills";
+  version = "1.0.0";
+  src = lib.cleanSource ../../.;
+
+  cargoLock.lockFile = ../../Cargo.lock;
+
+  doCheck = false;
+
+  buildPhase = ''
+    runHook preBuild
+    cargo build --release ${lib.concatMapStrings (crate: " -p " + crate) skillCrates}
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    ${lib.concatMapStringsSep "\n" (
+      bin: "install -Dm755 target/release/${bin} $out/bin/${bin}"
+    ) skillBins}
+    runHook postInstall
+  '';
+}
+
